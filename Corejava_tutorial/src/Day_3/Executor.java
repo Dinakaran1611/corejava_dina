30acb8aa838e634abf3ee5ebfcb6d2b9f143c52c
HARIHARAN S <HARIHARAN S@DESKTOP-MPDTAT4>
2024-03-07 14:07:24 +0530

    two customer example

--- Corejava_tutorial/src/Day_3/Executor.java
+++ Corejava_tutorial/src/Day_3/Executor.java
@@ -2,10 +2,23 @@ package Day_3;
 
 public class Executor {
 	public static void main (String[] args) {
+		
 		Customer c1= new Customer();
 		c1.setCid(112);
 		c1.setName("dina");
 		c1.setAddress("pondy");
-		System.out.println("customer id is "+c1.getCid()+" customer name "+c1.getName()+" customer address "+c1.getAddress());
+		System.out.println("customer id is "+c1.getCid());
+		System.out.println("customer name "+c1.getName());
+		System.out.println("customer address "+c1.getAddress());
+				
+				
+		Customer c2= new Customer();
+		c2.setCid(113);
+		c2.setName("dhiviya");
+		c2.setAddress("jammu kashmir");
+		System.out.println("customer id is "+c2.getCid());
+		System.out.println("customer name "+c2.getName());
+		System.out.println("customer address "+c2.getAddress());
+		
 	}
 }
